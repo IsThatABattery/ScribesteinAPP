90bf7e197d4eb8279009951b01862cc7b42ffe7d
jvolchyok <joshua.volchyok@gmail.com>
2025-08-12 17:03:01 +0200

    Initial Commit

---
+++ ScribesteinAPP/ScribesteinAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScribesteinAPPApp.swift
+//  ScribesteinAPP
+//
+//  Created by Joshua Volchyok on 8/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ScribesteinAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
